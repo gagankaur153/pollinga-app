20e697b8202f54f45e3416686fc377828dc18d63
gagankaur153 <gagankaur672000@gmail.com>
2026-03-29 18:57:32 +0530

    update frontend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+const url = window.location.origin.includes("localhost")
+    ? "http://localhost:5000/api"
+    : "https://polling-system-det7.vercel.app/api"
+
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:url,
   withCredentials: true
 });
 
